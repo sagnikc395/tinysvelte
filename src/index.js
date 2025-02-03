43018afc11bb1b7e886ad2340e230136d6da0904
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-02-03 16:23:25 +0530

    src: added generate ast

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import parse from './parser';
 import analyze from './analyze';
-
+import generate from './generate';
 
 const content = fs.readFileSync('./app.svelte', 'utf-8');
 const ast = parse(content);
@@ -11,5 +11,3 @@ const js = generate(ast, analysis);
 //fs.writeFileSync('./app.js', js, 'utf-8');
 fs.writeFileSync('./app.js', js, 'utf-8');
 
-
-function generate(ast, analysis) { }
