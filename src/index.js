505896299493e66e3d2a39b23d848b4093cba9ea
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-02-03 11:32:51 +0530

    src: boilerplate code overview

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import * as fs from 'fs';
+import { analyze } from 'periscopic';
+
+const content = fs.readFileSync('./app.svelte', 'utf-8');
+const ast = parse(content);
+const analysis = analyze(ast);
+const js = generate(ast, analysis);
+
+fs.writeFileSync('./app.js', js, 'utf-8');
+
+function parse(content) { }
+function analyse(ast) { }
+function generate(ast, analysis) { }
