fd2ebb783f8a860c2205fe285a20241894e46c6d
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-03-22 15:18:27 +0530

    refactor

--- src/app.svelte
+++ src/app.svelte
@@ -1,9 +1,8 @@
 <script>
-    let counter = 0;
-    const increment = () => counter++;
-    const decrement = () => counter--;
+  let counter = 0;
+  const increment = () => counter++;
+  const decrement = () => counter--;
 </script>
-
 
 <button on:click={decrement}>Decrement</button>
 {counter}
