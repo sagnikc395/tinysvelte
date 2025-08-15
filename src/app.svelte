b3a72439b163a995125c22a7b9c50efc6b96711b
sagnik <sagnikchatterjee607@gmail.com>
2025-08-15 13:07:53 +0530

    start again

--- src/app.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  let counter = 0;
-  const increment = () => counter++;
-  const decrement = () => counter--;
-</script>
-
-<button on:click={decrement}>Decrement</button>
-{counter}
-<button on:click={increment}>Increment</button>
