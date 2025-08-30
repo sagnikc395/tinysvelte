2b92f0b18cc07647d56c5f948b34912e9a8ea73f
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-08-30 10:50:00 -0400

    feat: added methods for parsing and their utils

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-import { generate } from 'escodegen';
-import * as fs from 'fs';
+import { generate } from "escodegen";
+import * as fs from "fs";
 
-// what our compiler will do 
-const content = fs.readFileSync('./app.svelte','utf-8');
+// what our compiler will do
+const content = fs.readFileSync("./app.svelte", "utf-8");
 const ast = parse(content);
 const analysis = analyse(ast);
-const js_gen = generate(ast,analysis);
+const js_gen = generate(ast, analysis);
 
-//write the results back to app.js 
-fs.writeFileSync('./app.js',js_gen,'utf-8');
+//write the results back to app.js
+fs.writeFileSync("./app.js", js_gen, "utf-8");
 
+function parse(content) {
+  let i = 0;
+  const ast = {};
+  ast.html = parseFragments();
 
-function parse(content){}
-function analyse(ast){}
-function generate(ast,analysis){}
+  return ast;
+
+  function pasrseFraments() {}
+  function parseFragment() {}
+  function parseScript() {}
+  function parseElement() {}
+  function parseAttributeList() {}
+  function parseExpression() {}
+  function parseText() {}
+  function parseJavascript() {}
+
+  //utility for the parser
+  function match(str) {
+    //slice out the characters and check if it matches
+    return content.slice(i, i + str.length) === str;
+  }
+
+  function eat(str) {
+    //if it does not match then syntax error
+    if (match(str)) {
+      i += str.length;
+    } else {
+      throw new Error(`Parse error: expecting "${str}"`);
+    }
+  }
+
+  //keep of matching until we have read so far
+  function readWhileMatching(regex) {
+    let startIndex = i;
+    while (regex.test(content[i])) {
+      i++;
+    }
+    return content.slice(startIndex, i);
+  }
+}
+function analyse(ast) {}
+function generate(ast, analysis) {}
